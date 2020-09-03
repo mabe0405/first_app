635d8b0481401f3ccf2bb7c98f92eb72dfff10eb
mabe0405 <m.abe.prog@gmail.com>
2020-09-03 20:44:59 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+
+	def index  # indexアクションを定義した
+		
+	end
+
+	def new
+	  
+	end
+
+	def create
+		Post.create(content: params[:content])
+	end
+
+
+
+end
